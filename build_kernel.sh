ff352674e441d63212d35f55eba224c5ae6f62cb
adhitya1978 <53357449+adhitya1978@users.noreply.github.com>
2019-08-10 20:06:57 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/home/dhanny/kerneldev/android/toolchains/android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=../arm-eabi-4.7/bin/arm-eabi-
 
 make VARIANT_DEFCONFIG=msm8916_sec_gt5note8lte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j
