e8a9f5d14cf06776446102dd8c6e25c396565a4a
adhitya1978 <adhitya_1978@ymail.com>
2019-08-10 19:48:35 +0700

    First commit

---
+++ init/version.c
@@ -0,0 +1,57 @@
+/*
+ *  linux/init/version.c
+ *
+ *  Copyright (C) 1992  Theodore Ts'o
+ *
+ *  May be freely distributed as part of Linux.
+ */
+
+//#include <generated/compile.h>
+#include <linux/module.h>
+#include <linux/uts.h>
+#include <linux/utsname.h>
+#include <generated/utsrelease.h>
+#include <linux/version.h>
+#include <linux/proc_ns.h>
+
+#ifndef CONFIG_KALLSYMS
+#define version(a) Version_ ## a
+#define version_string(a) version(a)
+
+extern int version_string(LINUX_VERSION_CODE);
+int version_string(LINUX_VERSION_CODE);
+#endif
+
+#define UTS_MACHINE "arm"
+#define LINUX_COMPILE_HOST "SWDD6306"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Dec 13 23:46:37 WIB 2018"
+#define LINUX_COMPILE_BY "PERTECH"
+#define LINUX_COMPILE_HOST "SWDD6306"
+#define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.03) "
+
+struct uts_namespace init_uts_ns = {
+	.kref = {
+		.refcount	= ATOMIC_INIT(2),
+	},
+	.name = {
+		.sysname	= UTS_SYSNAME,
+		.nodename	= UTS_NODENAME,
+		.release	= UTS_RELEASE,
+		.version	= UTS_VERSION,
+		.machine	= UTS_MACHINE,
+		.domainname	= UTS_DOMAINNAME,
+	},
+	.user_ns = &init_user_ns,
+	.proc_inum = PROC_UTS_INIT_INO,
+};
+EXPORT_SYMBOL_GPL(init_uts_ns);
+
+/* FIXED STRINGS! Don't touch! */
+const char linux_banner[] =
+	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
+	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+
+const char linux_proc_banner[] =
+	"%s version %s"
+	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (" LINUX_COMPILER ") %s\n";
